911c04eed5f1bf9b5a7c06ffca34c072d54d6fe8
Reza <ralmassizx@gmail.com>
2026-04-13 01:44:42 +0200

    changes to main of preparation and segregation

--- ingestion/main.py
+++ ingestion/main.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description="A script that initialize the Ingestion system orchestrator")
 
-    parser.add_argument("--testing_mode", type=bool, default=False, help="Launch the orchestrator in testing mode to generate logs (optional)")
+    parser.add_argument("--testing_mode", type=bool, default=True, help="Launch the orchestrator in testing mode to generate logs (optional)")
     parser.add_argument("--config_file_path", type=str, default=None, help="path of the configuration file (optional)")
 
     args = parser.parse_args()
 
-    orchestrator = IngestionSystemOrchestrator(args.config_file_path,args.testing_mode)
+    orchestrator = IngestionSystemOrchestrator(args.config_file_path)
     orchestrator.start()

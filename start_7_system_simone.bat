15226cc894ba27635b3bcc87fc6c897374054c7d
Reza <ralmassizx@gmail.com>
2026-04-13 14:53:08 +0200

    updating starting 7 systems

--- start_7_system_simone.bat
+++ start_7_system_simone.bat
@@ -7,13 +7,13 @@ if "%ROOT:~-1%"=="\" set "ROOT=%ROOT:~0,-1%"
 echo Cleaning previous log files...
 if exist "%ROOT%\logs\*.json" del /q "%ROOT%\logs\*.json"
 
-:: Launching all systems in separate windows
-start "Client Side" cmd /k "cd /d "%ROOT%\client_side" && python main.py"
-start "Ingestion" cmd /k "cd /d "%ROOT%" && python ingestion_launcher.py"
-start "Preparation" cmd /k "cd /d "%ROOT%" && python preparation_launcher.py"
-start "Ingestion System" cmd /k "cd /d "%ROOT%\ingestion" && python ingestion_launcher.py"
-start "Development" cmd /k "cd /d "%ROOT%\development" && python main.py"
-start "Production" cmd /k "cd /d "%ROOT%\production" && python main.py"
-start "Evaluation" cmd /k "cd /d "%ROOT%\evaluation\src" && python main.py"
+wt ^
+new-tab --title "Client Side" cmd /k "cd /d "%ROOT%\client_side" && python main.py" ; ^
+new-tab --title "Ingestion" cmd /k "cd /d "%ROOT%" && python -m ingestion.main" ; ^
+new-tab --title "Preparation" cmd /k "cd /d "%ROOT%" && python -m preparation.main" ; ^
+new-tab --title "Segregation" cmd /k "cd /d "%ROOT%\ingestion" && python main.py" ; ^
+new-tab --title "Development" cmd /k "cd /d "%ROOT%\development" && python main.py" ; ^
+new-tab --title "Production" cmd /k "cd /d "%ROOT%\production" && python main.py" ; ^
+new-tab --title "Evaluation" cmd /k "cd /d "%ROOT%\evaluation\src" && python -m src.main"
 
 endlocal
